3d8c7fc3dfa1d4883a50a5176456a75192ae3a05
lephuhao24 <32507309+lephuhao24@users.noreply.github.com>
2019-12-23 14:57:52 +0700

    almost done evt

--- frw_hvcg/dist/js/index.js
+++ frw_hvcg/dist/js/index.js
@@ -1,24 +1,24 @@
 "use strict";
 
-$('.icon').click(function () {
-  $('ul.menu-btn').toggleClass('active');
-});
-$('.link-page').click(function () {
-  if ($(this).parent().hasClass('active')) {
-    $(this).parent().removeClass('active');
-  } else {
-    $(this).parent().parent().children('li').removeClass('active');
-    $(this).parent().addClass('active');
-  }
-});
-$(window).scroll(function () {
-  if ($(window).scrollTop() < 500) {
-    $('.nav').css('position', 'static');
-  } else {
-    $('.nav').css('position', 'fixed');
-  }
-});
 $(document).ready(function () {
+  $('.icon').click(function () {
+    $('ul.menu-btn').toggleClass('active');
+  });
+  $('.link-page').click(function () {
+    if ($(this).parent().hasClass('active')) {
+      $(this).parent().removeClass('active');
+    } else {
+      $(this).parent().parent().children('li').removeClass('active');
+      $(this).parent().addClass('active');
+    }
+  });
+  $(window).scroll(function () {
+    if ($(window).scrollTop() < 500) {
+      $('.nav').css('position', 'static');
+    } else {
+      $('.nav').css('position', 'fixed');
+    }
+  });
   $('.cards-team').slick({
     infinite: true,
     slidesToShow: 4,
@@ -52,6 +52,89 @@ $(document).ready(function () {
       settings: {
         infinite: true,
         slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+  $('.cards-recent').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+    dots: true,
+    arrows: false,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3
+      }
+    }, {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }]
+  });
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    arrows: false,
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    responsive: [{
+      breakpoint: 1200,
+      settings: {
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }, {
+      breakpoint: 480,
+      settings: {
+        infinite: true,
+        slidesToShow: 2,
         slidesToScroll: 1
       }
     }]
--- frw_hvcg/src/js/index.js
+++ frw_hvcg/src/js/index.js
@@ -1,25 +1,26 @@
-$('.icon').click(function(){
-    $('ul.menu-btn').toggleClass('active')
-});
-
-$('.link-page').click(function(){
-    if($(this).parent().hasClass('active')){
-        $(this).parent().removeClass('active');
-    } else {
-        $(this).parent().parent().children('li').removeClass('active');
-        $(this).parent().addClass('active');
-    }
-});
-
-$(window).scroll(function() {
-   if($(window).scrollTop() < 500){
-        $('.nav').css('position', 'static');
-   }
-   else {
-    $('.nav').css('position', 'fixed');
-   }
-});
 $(document).ready(function(){
+    $('.icon').click(function(){
+        $('ul.menu-btn').toggleClass('active')
+    });
+    
+    $('.link-page').click(function(){
+        if($(this).parent().hasClass('active')){
+            $(this).parent().removeClass('active');
+        } else {
+            $(this).parent().parent().children('li').removeClass('active');
+            $(this).parent().addClass('active');
+        }
+    });
+    
+    $(window).scroll(function() {
+       if($(window).scrollTop() < 500){
+            $('.nav').css('position', 'static');
+       }
+       else {
+        $('.nav').css('position', 'fixed');
+       }
+    });
+    
     $('.cards-team').slick({
         infinite: true,
         slidesToShow: 4,
@@ -61,5 +62,98 @@ $(document).ready(function(){
                 }
             }
         ]
-    })
+    });
+    $('.cards-recent').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        adaptiveHeight: true,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 })
